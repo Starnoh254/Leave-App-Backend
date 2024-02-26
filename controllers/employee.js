83829bec8fefb3293101da45e04f5c7082c0e41a
Starnoh254 <stajohn697@gmail.com>
2024-02-26 10:41:49 +0300

    created mongodb connection

--- controllers/employee.js
+++ controllers/employee.js
@@ -1,7 +1,21 @@
-function employeeSignin(req , res){
+const Employee = require('../models/employeeModel')
 
-}
+function employeeSignin(req, res) {
+  const { email, password } = req.body;
+  Employee.findOne({ email }, (err, user) => {
+    if (!user) return res.status(401).send({ error: 'Email not found.' });
+    // compare the password of this user with the password that was sent in the request body
+    if (user.password !== password) return res.status(401).send({ error: 'Invalid Password'});
+    else{
+        return res.status(200).send({
+            message:'User signed in successfully',
+            user
+    })
+  }
 
-function applyForLeave(req , res){
-    // endpoint for processing employee leave application
+})}
+
+function applyForLeave(req, res) {
+  // endpoint for processing employee leave application
+  
 }
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const mongoose = require('mongoose')
 
 const PORT = 3000;
 const app = express();
+
+
+mongoose.connect(
+  "mongodb://127.0.0.1:27017/leave?directConnection=true&serverSelectionTimeoutMS=2000"
+).then(() => {
+  console.log("MongoDB Connected Successfully!");
+}).catch((err) => console.error(err));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
